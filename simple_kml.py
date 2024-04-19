5839ede2e9fffc032540cb76c11cb75d5e9fe755
Landon <6965317+landonas@users.noreply.github.com>
2024-04-19 00:03:08 -1000

    Update simple_kml.py
    slight fixes for style after testing

--- simple_kml.py
+++ simple_kml.py
@@ -1,7 +1,9 @@
 import simplekml
 
 kml = simplekml.Kml()
-bangahz_only = kml.newpoint(name="Pathways", coords=[(21.331575, -158.065298)]) #lat, lon
+bangahz_only = kml.newpoint(name="Pathways", coords=[( -158.065, 21.331)]) 
+bangahz_only.style.labelstyle.color = "ff0000ff"
+bangahz_only.style.iconstyle.href='http://maps.google.com/mapfiles/kml/pushpin/blue-pushpin.png'
 #test output
-print(kml.kml())
+#print(kml.kml())
 kml.save("808_bangahz_only.kml")
